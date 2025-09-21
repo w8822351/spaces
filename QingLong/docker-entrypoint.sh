183af52bc6e5f8cc297fc7d5741dcc243af128fb
w8822351 <73152983+w8822351@users.noreply.github.com>
2025-09-21 23:33:34 +0800

    Update docker-entrypoint.sh

--- QingLong/docker-entrypoint.sh
+++ QingLong/docker-entrypoint.sh
@@ -79,7 +79,7 @@ init_auth_info "\"username\": \"$ADMIN_USERNAME\", \"password\": \"$ADMIN_PASSWO
 if [ -n "$RCLONE_CONF" ]; then
   echo -e "##########同步备份############"
   # 指定远程文件夹路径，格式为 remote:path
-  REMOTE_FOLDER="huggingface:/qinglong"
+  REMOTE_FOLDER="huggingface:/qinglong_w8822351"
 
   # 使用 rclone ls 命令列出文件夹内容，将输出和错误分别捕获
   OUTPUT=$(rclone ls "$REMOTE_FOLDER" 2>&1)
